021f2cef5b451d6736e797259fd728f62604de2a
Alexander Kurganov <a.kurganow@gmail.com>
2023-09-17 23:13:32 +0400

    Refactoring

--- src/components/Dialog/index.stories.tsx
+++ src/components/Dialog/index.stories.tsx
@@ -34,8 +34,7 @@ const meta: Meta<ComponentProps<typeof Dialog>> = {
 				{story()}
 			</div>
 	],
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	render: ({ ...args }) =>
+	render: () =>
 		<div style={{ width: '25rem' }}>
 			<DialogHeader>Dialog</DialogHeader>
 
