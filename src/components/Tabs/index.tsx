58b4addb5534855fc4389294463cbd953c118437
Alexander Kurganov <a.kurganow@gmail.com>
2023-09-25 02:47:22 +0400

    Fix Tabs width
    Signed-off-by: Alexander Kurganov <a.kurganow@gmail.com>

--- src/components/Tabs/index.tsx
+++ src/components/Tabs/index.tsx
@@ -15,7 +15,10 @@ interface TabsProps extends HTMLAttributes<HTMLDivElement> {
 
 export default function Tabs({ items, ...props }: TabsProps) {
 	const contentRef = useRef<HTMLDivElement>(null)
-	const [contentHeight, setContentHeight] = useState<number>()
+	const [
+		contentSize,
+		setContentSize,
+	] = useState<{ width: number, height: number }>()
 	const [activeTab, setActiveTab] = useState(0)
 	const isTabActive = useCallback((index: number) => {
 		return index === activeTab
@@ -31,66 +34,82 @@ export default function Tabs({ items, ...props }: TabsProps) {
 		if (!content) return
 
 		const children = Array.from(content.children)
-		const childrenHeights = children.map(child => child.getBoundingClientRect().height)
-		const childrenMaxHeight = Math.max.apply(null, childrenHeights)
+		const childrenRects = children.map(child => child.getBoundingClientRect())
+		const childrenMaxHeight = Math.max.apply(null, childrenRects.map(rect => rect.height))
+		const childrenMaxWidth = Math.max.apply(null, childrenRects.map(rect => rect.width))
 		const childrenHeight = Math.ceil(childrenMaxHeight)
+		const childrenWidth = Math.ceil(childrenMaxWidth)
 
-		setContentHeight(childrenHeight)
+		console.log('boundiWidth', childrenRects.map(rect => rect.width))
+		console.log('boundiHeight', childrenRects.map(rect => rect.height))
+
+		setContentSize({
+			width: childrenWidth,
+			height: childrenHeight,
+		})
 	}, [contentRef])
 
 	return (
-		<div {...props} className={st.container} data-testid="Tabs">
+		<>
 			<style>{
-				contentHeight
-					? `.${st.content} { --tab-content-height: min(${contentHeight}px, 50vh); }`
+				contentSize
+					? `
+					.${st.content} {
+					--tab-content-height: min(${contentSize.height}px, 50vh);
+					--tab-content-width: min(${contentSize.width}px, 75vw);
+					}`
 					: ''
 			}</style>
-			<div className={st.header}>
-				{items.map((item, index) => {
-					const { title, disabled } = item
-					const classNames = cn(st.headerItem, {
-						[st.active]: isTabActive(index),
-					})
-					return (
-						<button
-							key={index}
-							className={classNames}
-							onClick={disabled ? undefined : handleTabSwitch(index)}
-							disabled={disabled}
-							data-testid="Tabs:Item"
-							tabIndex={0}
-						>
-							{title}
-						</button>
-					)
-				})}
-			</div>
+			<div {...props} className={st.container} data-testid="Tabs">
+				<div className={st.header}>
+					{items.map((item, index) => {
+						const { title, disabled } = item
+						const classNames = cn(st.headerItem, {
+							[st.active]: isTabActive(index),
+						})
+						return (
+							<button
+								key={index}
+								className={classNames}
+								onClick={disabled ? undefined : handleTabSwitch(index)}
+								disabled={disabled}
+								data-testid="Tabs:Item"
+								tabIndex={0}
+								title={typeof title === 'string' ? title : undefined}
+							>
+								{title}
+							</button>
+						)
+					})}
+				</div>
 
-			<div
-				className={st.content}
-				data-testid="Tabs:Content"
-				ref={contentRef}
-			>
-				{items.map((item, index) => {
-					const { children } = item
-					const isActive = isTabActive(index)
-					const classNames = cn(st.contentItem, {
-						[st.active]: isActive,
-					})
+				<div
+					className={st.content}
+					data-testid="Tabs:Content"
+					ref={contentRef}
+				>
+					{items.map((item, index) => {
+						const { children } = item
+						const isActive = isTabActive(index)
+						const classNames = cn(st.contentItem, {
+							[st.active]: isActive,
+							[st.preflight]: !contentSize,
+						})
 
-					return (
-						<div
-							key={index}
-							className={classNames}
-							data-testid={isActive ? 'Tabs:ContentItem' : undefined}
-							data-index={index}
-							tabIndex={isActive ? 0 : -1}
-						>
-							{(!contentHeight || isActive) && children}
-						</div>
-					)
-				})}
+						return (
+							<div
+								key={index}
+								className={classNames}
+								data-testid={isActive ? 'Tabs:ContentItem' : undefined}
+								data-index={index}
+								tabIndex={isActive ? 0 : -1}
+							>
+								{(!contentSize || isActive) && children}
+							</div>
+						)
+					})}
+				</div>
 			</div>
-		</div>
+		</>
 	)
 }
