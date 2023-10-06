0a0340f945deabc8f53a957a64529ae0bbb1f897
Alexander Kurganov <a.kurganow@gmail.com>
2023-10-06 15:45:00 +0400

    Update jest workflow
    Signed-off-by: Alexander Kurganov <a.kurganow@gmail.com>

--- jest.config.ts
+++ jest.config.ts
@@ -8,7 +8,11 @@ import type { Config } from 'jest'
 const config: Config = {
 	rootDir: './src',
 	preset: 'ts-jest/presets/js-with-ts',
-	globals: {},
+	globals: {
+		'ts-jest': {
+			tsConfig: 'tsconfig.jest.json'
+		}
+	},
 
 	// The paths to modules that run some code to configure or set up the testing environment before each test
 	setupFiles: [
